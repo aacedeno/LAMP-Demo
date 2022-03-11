145721bf3575ec61399e54ef5b6f73fce5dc6928
Adrian Cedeno <65040149+aacedeno@users.noreply.github.com>
2022-03-10 19:42:41 -0500

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -2,17 +2,17 @@
 
 #Description
 #Script is used to download and configure ufw (firewall manager)
-#Opens port 22,80,443 and enables the firewall which turns on the service
+#Opens port 22,80,443 and enables the firewall rules 
 #Last command shows status of the rules in affect 
 
 
 
-Sudo apt install ufw
-Sudo Ufw default deny incoming 
-Sudo Ufw default all outgoing
+sudo apt install ufw
+sudo Ufw default deny incoming 
+sudo Ufw default all outgoing
 sudo ufw allow ssh
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo ufw enable
 sudo systemctl status ufw
-Sudo ufw status 
+sudo ufw status 
